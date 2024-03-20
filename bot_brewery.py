11a2f5e93393e44db6bf35462b34da1e0bf0ea1b
krammarkro <81025538+krammarkro@users.noreply.github.com>
2024-03-20 20:36:16 +0100

    first commit

---
+++ bot_brewery.py
@@ -0,0 +1,278 @@
+import discord
+from discord import app_commands
+from discord.ext import commands
+import sheet as sh
+import logging
+import inspect
+import sys
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+TOKEN= os.getenv("TOKEN") # Discord Token
+print(TOKEN)
+role_dict = {
+    "RP": ("B", "C"),
+    "TL": ("D", "E"),
+    "PR": ("F", "G"),
+    "CLRD": ("H", "I"),
+    "TS": ("J", "K"),
+    "QC": ("L", "M")
+}
+role_dict_reaction = {
+    "B": "RP",
+    "D": "TL",
+    "F": "PR",
+    "H": "CLRD",
+    "J": "TS",
+    "L": "QC"
+}
+logging.basicConfig(level=logging.INFO)
+logging.basicConfig(filename='bot_errors.log', level=logging.ERROR)
+line_number = inspect.currentframe().f_lineno
+
+def handle_exception(exc_type, exc_value, exc_traceback):
+    if issubclass(exc_type, KeyboardInterrupt):
+        sys.__excepthook__(exc_type, exc_value, exc_traceback)
+        return
+
+    logging.error("Uncaught exception", exc_info=(exc_type, exc_value, exc_traceback))
+
+sys.excepthook = handle_exception
+
+
+
+bot = commands.Bot(command_prefix='$', intents=discord.Intents.all())
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user} has connected to Discord!')
+    try: 
+        synced = await bot.tree.sync()
+        print(f"Synced {len(synced)} command(s)")
+    except Exception as e:
+        print(e)
+
+
+async def remove_reaction(channel_id, message_id, emoji):
+    channel = bot.get_channel(channel_id)
+    message = await channel.fetch_message(message_id)
+    await message.clear_reaction(emoji)
+
+async def delete_message(channel_id, message_id):
+    channel = bot.get_channel(int(channel_id))
+
+    message = await channel.fetch_message(int(message_id))
+
+    await message.delete()
+
+@bot.event
+async def on_raw_reaction_add(payload):
+    print(payload.emoji)
+    channel_id = payload.channel_id
+    target_channel_id = 1218705159614631946
+    if ((channel_id == target_channel_id) and payload.user_id != 1218682240947458129):
+        print("Passed channel and not bot ")
+        data, row_name = sh.getmessageid(payload.message_id)
+        print(data[4])
+        print(payload.user_id)
+        if f"<@{payload.user_id}>" == data[4]:
+            print("passed right user")
+            print(repr(payload.emoji))
+            emoji = "<PartialEmoji animated=False name='✅' id=None>"
+            if repr(payload.emoji) == emoji:
+                print("passed right emoji")
+                print("Reaction added in the target channel")
+                print(payload)
+                #data, row_name = sheet.getmessageid(payload.message_id)
+                #data = sheet.getmessageid(payload.message_id)
+                print(data)
+                series = sh.getchannelid(data[0])
+                #series = datatest.get_key("channel.json", data[0])
+                chapter = data[1]
+                role = data[2]
+                if role in role_dict_reaction:
+                    role = role_dict_reaction[role]
+                else:
+                    logging.info(f"Invalid role at line {line_number}")
+
+                user= data[4]
+                sh.write(data, "Claimed")
+                await remove_reaction(payload.channel_id, payload.message_id, "❌")
+                await remove_reaction(payload.channel_id, payload.message_id, "✅")
+                target_channel_id = int("1219030657955794954")  # change to actual channel 
+                target_channel = bot.get_channel(target_channel_id)
+                await target_channel.send(f"{series} | CH {chapter} | {role} | sheet updated to status: Accepted by: {user}")
+            
+            elif repr(payload.emoji) == "<PartialEmoji animated=False name='🥂' id=None>":
+                print("test")
+                role = None
+                target_channel_id = int("1218705159614631946")  # change to actual channel 
+                target_channel = bot.get_channel(target_channel_id)
+                data, row_name = sh.getmessageid(payload.message_id)
+                #data = sheet.getmessageid(payload.message_id)
+                series = sh.getchannelid(data[0])
+                #series = datatest.get_key("channel.json", data[0])
+                chapter = data[1]
+                role = data[2]
+                if role in role_dict_reaction:
+                    role = role_dict_reaction[role]
+                else:
+                    logging.info(f"Invalid role at line {line_number}")
+
+                user= data[4]
+                print("this is series")
+                print(series)
+                print(role)
+                await target_channel.send(f"{series} | CH {chapter} | {role} | Done | {user}")
+                #await delete_message(payload.channel_id, payload.message_id)
+                sh.write(data, "Done")
+                sh.delete_row(row_name) #clear
+            
+
+            else: 
+                target_channel_id = int("1219030657955794954")  # Replace with the ID of the target channel
+                target_channel = bot.get_channel(target_channel_id)
+                data, row_name = sh.getmessageid(payload.message_id)
+                #data = sheet.getmessageid(payload.message_id)
+                series = sh.getchannelid(data[0])
+                #series = datatest.get_key("channel.json", data[0])
+                chapter = data[1]
+                role = data[2]
+                if role in role_dict_reaction:
+                    role = role_dict_reaction[role]
+                else:
+                    logging.info(f"Invalid role at line {line_number}")
+
+                user= data[4]
+                
+                sh.write(data, "Declined")
+                await target_channel.send(f"{series} | CH {chapter} | {role} | sheet updated to status: Declined by: {user}")
+                await delete_message(payload.channel_id, payload.message_id)
+                print(row_name)
+                sh.delete_row(row_name) #clear 
+    else: 
+        print("Wrong channel")
+
+
+@bot.tree.command(name="findid")    
+@app_commands.describe(user = "User")
+async def findid(interaction : discord.Interaction, user: discord.User):
+    await interaction.response.send_message("Done")
+    sh.findid(user.name, str(user.id))
+
+
+@bot.event
+async def on_member_join(member):
+    print("new member")
+    print(member.name)
+    print(member.id)
+    sh.findid(member.name, str(member.id))
+    #await channel.send(f"{member} has arrived!")
+    
+@bot.tree.command(name="say")
+@app_commands.describe(arg = "what to say")
+async def say(interaction : discord.Interaction, arg: str):
+    user = interaction.user.name
+    await interaction.response.send_message(f"Hey{interaction.user.mention}, test 2, {user}")
+
+
+@bot.tree.command(name="assign")
+@app_commands.describe(series = "# of the series", chapter = "What chapter", role = "What needs to be done", who = "Who")
+async def assign(interaction : discord.Interaction, series :str, chapter :str, role :str, who: str):
+    print("assign")
+    first = None
+    second = None 
+    target_channel_id = int("1218705159614631946")  # Replace with the ID of the target channel
+    target_channel = bot.get_channel(target_channel_id)
+    role = role.upper()
+    if role.upper() in role_dict:
+        first, second = role_dict[role]
+        print(first)
+        print(second)
+    if first ==None:
+        await interaction.response.send_message(f" '{role.upper()}' is not a valid Role " , ephemeral=True)
+    else:
+        message =await target_channel.send(f"{series}| CH {chapter} | {role.upper()} | {who}")
+        await message.add_reaction("✅")
+        await message.add_reaction("❌")
+        await message.add_reaction("🥂")
+        #name = sheet.getuser(who)
+        print("done")
+        print(sh.getuser(who))
+        print(sh.getsheetname(series))
+        list= [sh.getsheetname(series), chapter, first, second, who]
+        sh.store(message.id, sh.getsheetname(series), chapter, who, first, second)
+        #sheet.store(message.id, sheet.getsheetname(series), chapter, sheet.getuser(who), first, second)
+        sh.write(list, "Assigned")
+        #await interaction.response.send_message("Assigned", ephemeral=True)
+
+@bot.tree.command(name="channel")
+@app_commands.describe(channel = "# of the channel", sheet= "Name of the sheet")
+async def channel(interaction : discord.Interaction, channel :str , sheet :str):
+    await interaction.response.send_message(f"{channel} was assigned to {sheet}")
+    print(channel)
+    #datatest.add_to_json_file('channel.json', sheet, channel)
+    sh.writechannel(channel, sheet)
+
+@bot.tree.command(name="create")
+@app_commands.describe(channelname = "Name of the channel", sheet= "Name of the sheet")
+async def create(interaction : discord.Interaction, channelname :str, sheet :str):
+    guild = interaction.guild
+    category = bot.get_channel(1218035431078236323)
+    sh.copy(sheet)
+
+    channel = await guild.create_text_channel(channelname, category=category)
+    new_position = 2  # Define new_position here
+    await channel.edit(position=new_position)
+    channels = sorted(guild.channels, key=lambda c: c.position)
+    channels_positions = {channels[i].id: i for i in range(len(channels))}
+    channels_positions[channel.id] = new_position
+    id = channel.id
+    sh.writechannel(f"<@{channel.id}>", sheet)
+
+
+@bot.tree.command(name="updatechannelname")
+@app_commands.describe(channel = "# of the Channel", new_name= "The new name for the sheet")
+async def updatechannelname(interaction : discord.Interaction, channel :str, new_name :str):
+    await interaction.response.send_message(f"{channel} was reassigned to {new_name}")
+    sh.updatesheet(channel, new_name)
+
+@bot.tree.command(name="updatechannelid")
+@app_commands.describe(new_channel = "new # of the Channel", name= "Name of the sheet")
+async def updatechannelid(interaction : discord.Interaction, new_channel :str, name :str):
+    await interaction.response.send_message(f"{name} was reassigned to {new_channel}")
+    sh.updatechannel_id(new_channel, name)
+
+@bot.command()
+async def foo(ctx, arg):
+    await ctx.send(arg)
+    
+
+@bot.tree.command(name= "done")
+@app_commands.describe(series= "# of the Series", chapter = "What chapter", role = "What role")
+async def assign(interaction : discord.Interaction, series :str, chapter : str, role :str):
+    first = None 
+    second = None
+    target_channel_id = int("1218705159614631946")  # change to actual channel 
+    target_channel = bot.get_channel(target_channel_id)
+    user = interaction.user.id 
+    print("now comes user bevore check" )
+    print(user)
+    
+    role = role.upper()
+    if role.upper() in role_dict:
+        first, second = role_dict[role]
+    if first == None:
+        if target_channel is None:
+            print("Target channel is None")
+        else:
+            await interaction.response.send_message(f" '{role.upper()}' is not a valid Role ", ephemeral=True)       
+    else:        
+        #await target_channel.send(f"{interaction.user.mention} | {series}| CH {chapter} | {role.upper()} | Done")
+        await target_channel.send(f"{series} | CH {chapter} | {role.upper()} | Done | {interaction.user.mention}")
+        list= [sh.getsheetname(series), chapter, first, second, f"<@{user}>"]   
+        sh.write(list, "Done")
+        #await interaction.response.send_message("Send", ephemeral=True)
+
+bot.run(TOKEN)
---
+++ sheet.py
@@ -0,0 +1,265 @@
+import os 
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build 
+from googleapiclient.errors import HttpError
+from googleapiclient.discovery import build
+from dotenv import load_dotenv
+
+SCOPES = ["https://www.googleapis.com/auth/spreadsheets"] 
+staffsheet= os.getenv("staffsheet")
+datasheet= os.getenv("datasheet")
+SPREADSHEET_ID = os.getenv("SPREADSHEET_ID")
+credential = Credentials.from_authorized_user_file("token.json", SCOPES)
+
+def channelid(channel, name):
+    credential = Credentials.from_authorized_user_file("token.json", SCOPES)
+    
+    try:
+        service = build("sheets", "v4", credentials=credential)
+        sheets = service.spreadsheets()
+        sheets.values().append(spreadsheetId=staffsheet, insertDataOption="INSERT_ROWS" , range=f"3:3", valueInputOption="USER_ENTERED", body={'values': [[name, "","","","","","","","","","","","","","","","","", "","", "", id]]}).execute()
+    except HttpError as error:
+        print(error)
+
+
+def copy(name):
+    checkcred()
+    try:
+        service = build("sheets", "v4", credentials=credential)
+        sheets = service.spreadsheets()
+
+        # Get the spreadsheet
+        spreadsheet = sheets.get(spreadsheetId=datasheet).execute()
+
+        # Get the worksheet you want to copy
+        template_sheet = [s for s in spreadsheet['sheets'] if s['properties']['title'] == 'template'][0]
+
+        # Duplicate the worksheet
+        request = {
+            'requests': [
+                {
+                    'duplicateSheet': {
+                        'sourceSheetId': template_sheet['properties']['sheetId'],
+                        'newSheetName': name,
+                    }
+                }
+            ]
+        }
+        sheets.batchUpdate(spreadsheetId=datasheet, body=request).execute()
+
+        # Append values to the new sheet
+        sheets.values().update(spreadsheetId=datasheet, range=f"{name}!A1:A1", valueInputOption="USER_ENTERED", body={'values': [[name]]}).execute()
+        #sheets.values().append(
+        #    spreadsheetId=datasheet,
+        #    insertDataOption="INSERT_ROWS",
+        #    range=f"3:3",
+        #    valueInputOption="USER_ENTERED",
+        #    body={'values': [[name, "","","","","","","","","","","","","","","","","", "","", "", id]]}
+        #).execute()
+    except HttpError as error:
+        print(error)
+
+def findid(name, id):
+    checkcred()
+
+    try: 
+        service = build("sheets", "v4", credentials=credential)
+        sheets = service.spreadsheets()
+        #if "<@" in id:
+        #    idd= id[2:-1]
+        #else: 
+        #    idd=id
+            #idd=id
+            #print(idd)üß
+        print(type(id))
+        #value= sheets.values().get(spreadsheetId=staffsheet, range=f"T:T").execute()
+        sheets.values().append(spreadsheetId=staffsheet, insertDataOption="INSERT_ROWS" , range=f"3:3", valueInputOption="USER_ENTERED", body={'values': [[name, "","","","","","","","","","","","","","","","","", "","", "", id]]}).execute()
+        #sheets.values().append(spreadsheetId=staffsheet, insertDataOption="INSERT_ROWS" , range=f"T13:T13", valueInputOption="USER_ENTERED", body={'values': [[id]]}).execute()
+
+    except HttpError as error:
+        print(error)
+
+def getuser(name):
+    sheet="STAFF"
+    checkcred()
+
+    try: 
+        service = build("sheets", "v4", credentials=credential)
+        sheets = service.spreadsheets()
+        value= sheets.values().get(spreadsheetId=staffsheet, range=f"V:V").execute()
+        print(value)
+        print(name)
+        for i, row in enumerate(value['values'], start=1):
+            if row and f"<@{row[0]}>" == f"{name}":
+                name= i
+
+        creditname= sheets.values().get(spreadsheetId=staffsheet, range=f"C{name}:C{name}").execute()
+        print(creditname)
+        return creditname["values"][0][0]
+    except HttpError as error:
+        print(error)
+
+def getmessageid(id):
+    name= None
+    checkcred()
+    try: 
+        service = build("sheets", "v4", credentials=credential)
+        sheets = service.spreadsheets()
+        value= sheets.values().get(spreadsheetId=datasheet, range=f"DATA!F:F").execute()
+        #print(value)
+        for i, row in enumerate(value['values'], start=1):
+            if row and f"{row[0]}" == f"{id}":
+                name= i
+                print(i)
+        if name is not None:
+            data= sheets.values().get(spreadsheetId=datasheet, range=f"DATA!G{name}:K{name}").execute()
+            print(data)
+            return data["values"][0], name
+    except HttpError as error:
+        print(error)
+
+
+def write(data, done):
+    sheet2 = data[0]
+    ch = data[1]
+    f = data[2]
+    se = data[3]
+    user = data[4]
+    print("here comes list ")
+    print(sheet2, ch, f, se, user, done) 
+    checkcred()
+
+
+
+
+
+    try: 
+        service = build("sheets", "v4", credentials=credential)
+        sheets = service.spreadsheets()
+        print(data)
+        value= sheets.values().get(spreadsheetId=datasheet, range=f"{sheet2}!A:A").execute()
+        print(value)
+        for i, row in enumerate(value['values'], start=1):
+            if row and row[0] == ch:
+                ch= i
+        print (user)       
+        value= sheets.values().get(spreadsheetId=datasheet, range=f"{sheet2}!{f}{ch}:{se}{ch}").execute()
+        sheets.values().update(spreadsheetId=datasheet, range=f"{sheet2}!{f}{ch}:{se}{ch}", valueInputOption="USER_ENTERED", body={'values': [[getuser(user),done]]}).execute()
+    except HttpError as error:
+        print(error)
+
+def store(message_id ,sheet, ch, user, f, se):
+    checkcred()
+    try: 
+        service = build("sheets", "v4", credentials=credential)
+        sheets = service.spreadsheets()
+        sheets.values().append(spreadsheetId=datasheet, range=f"DATA!F2:K2", insertDataOption="INSERT_ROWS",valueInputOption="USER_ENTERED", body={'values': [[str(message_id), sheet, ch, f, se, str(user)]]}).execute()
+    except HttpError as error:
+        print(error)
+
+
+def writechannel(channel_id, sheet):
+    checkcred()
+
+    try: 
+        service = build("sheets", "v4", credentials=credential)
+        sheets = service.spreadsheets()
+        
+        sheets.values().append(spreadsheetId=datasheet, insertDataOption="INSERT_ROWS" , range=f"CHANNELS!3:3", valueInputOption="USER_ENTERED", body={'values': [[channel_id, sheet]]}).execute()
+    except HttpError as error:
+        print(error)
+
+def updatesheet(channel_id, sheet):
+    checkcred()
+
+    try: 
+        service = build("sheets", "v4", credentials=credential)
+        sheets = service.spreadsheets()
+        
+        value= sheets.values().get(spreadsheetId=datasheet, range=f"CHANNELS!A:A").execute()
+        print(value)
+        for i, row in enumerate(value['values'], start=1):
+            if row and row[0] == f"{channel_id}":
+                row= i
+
+        sheets.values().update(spreadsheetId=datasheet, range=f"CHANNELS!{row}:{row}", valueInputOption="USER_ENTERED", body={'values': [[channel_id, sheet]]}).execute()
+    except HttpError as error:
+        print(error)
+
+def updatechannel_id(channel_id, sheet):
+    checkcred()
+
+    try: 
+        service = build("sheets", "v4", credentials=credential)
+        sheets = service.spreadsheets()
+        
+        value= sheets.values().get(spreadsheetId=datasheet, range=f"CHANNELS!B:B").execute()
+        print(value)
+        for i, row in enumerate(value['values'], start=1):
+            if row and row[0] == f"{sheet}":
+                row= i
+
+        sheets.values().update(spreadsheetId=datasheet, range=f"CHANNELS!{row}:{row}", valueInputOption="USER_ENTERED", body={'values': [[channel_id, sheet]]}).execute()
+    except HttpError as error:
+        print(error)
+
+
+def getsheetname(channel_id): 
+    checkcred()
+    try: 
+        service = build("sheets", "v4", credentials=credential)
+        sheets = service.spreadsheets()
+        rowd = None 
+        value= sheets.values().get(spreadsheetId=datasheet, range=f"CHANNELS!A:A").execute()
+        for i, row in enumerate(value['values'], start=1):
+            if row and row[0] == channel_id:
+                rowd= i 
+        name = sheets.values().get(spreadsheetId=datasheet, range=f"CHANNELS!B{rowd}:B{rowd}").execute()
+        return name["values"][0][0]
+    except HttpError as error:
+        print(error)
+
+
+def getchannelid(sheet):
+    checkcred()
+    try: 
+        service = build("sheets", "v4", credentials=credential)
+        sheets = service.spreadsheets()
+        rowd = None 
+        value= sheets.values().get(spreadsheetId=datasheet, range=f"CHANNELS!B:B").execute()
+        for i, row in enumerate(value['values'], start=1):
+            if row and row[0] == sheet:
+                rowd= i 
+        name = sheets.values().get(spreadsheetId=datasheet, range=f"CHANNELS!A{rowd}:A{rowd}").execute()
+        return name["values"][0][0]
+    except HttpError as error:
+        print(error)
+
+def delete_row(row_index):
+    checkcred()
+    service = build("sheets", "v4", credentials=credential)
+    sheets = service.spreadsheets()
+    sheets.values().update(spreadsheetId=datasheet, range=f"DATA!{row_index}:{row_index}", valueInputOption="USER_ENTERED", body={'values': [["", "","","","","","","","","","","","","","","","","", "","", "", ""]]}).execute()
+
+def get_sheet_id_by_name(spreadsheet_id, sheet_name):
+    service = build('sheets', 'v4', credentials=credential)
+    spreadsheet = service.spreadsheets().get(spreadsheetId=spreadsheet_id).execute()
+    for sheet in spreadsheet['sheets']:
+        if sheet['properties']['title'] == sheet_name:
+            
+            print(sheet['properties']['sheetId'])
+#get_sheet_id_by_name(datasheet, "DATA")
+
+def checkcred():
+    if os.path.exists("token.json"):
+        credential = Credentials.from_authorized_user_file("token.json", SCOPES)
+    if not credential or not credential.valid:
+        if credential and credential.expired and credential.refresh_token:
+            credential.refresh(Request())
+        else: 
+            flow = InstalledAppFlow.from_client_secrets_file("credentials.json", SCOPES)
+            credential = flow.run_local_server(port=0)
+            with open("token.json", "w") as token:
+                token.write(credential.to_json())
