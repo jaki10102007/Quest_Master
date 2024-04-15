dec82ede76bc3c80cac39ec1649f89c9d2d9cc71
Lialikelala <liaharv@gmail.com>
2024-04-15 17:37:53 -0400

    Update event_handlers.py

--- utils/event_handlers.py
+++ utils/event_handlers.py
@@ -27,12 +27,12 @@ async def on_ready(bot):
 
 async def on_disconnect(bot):
     logger.warning("Bot has disconnected from Discord.")
-    # ... rest of your on_ready code ...
+    # ... rest of your on_disconnect code ...
 
 
 async def on_resumed(bot):
     logger.info("Bot has reconnected and resumed operations.")
-    # ... rest of your on_disconnect code ...
+    # ... rest of your on_resumed code ...
 
 
 # Handles errors triggered during command invocation
