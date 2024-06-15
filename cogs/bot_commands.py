33d8c8118507ad245ea3b784abe13f94ac3b60a2
krammarkro <81025538+krammarkro@users.noreply.github.com>
2024-06-15 22:46:30 +0200

    Removed unused import

--- cogs/bot_commands.py
+++ cogs/bot_commands.py
@@ -1,7 +1,6 @@
 import discord
 from discord import app_commands
 from discord.ext import commands
-from discord import embeds
 import sheet as sh
 from requests import get
 from logger import setup_logger
