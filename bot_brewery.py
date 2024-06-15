067919ca4882e47f8d3bc5302b3f887cf90108b9
krammarkro <81025538+krammarkro@users.noreply.github.com>
2024-06-15 15:26:03 +0200

    made reactions their own function

--- bot_brewery.py
+++ bot_brewery.py
@@ -1,5 +1,6 @@
 import discord
-from config import TOKEN, BOT_ID, ASSIGNMENT_CHANNEL, CHECKUP_CHANNEL, ONESHOT_CHANNEL, COMMAND_PREFIX, role_dict, role_dict_reaction
+from config import TOKEN, BOT_ID, ASSIGNMENT_CHANNEL, CHECKUP_CHANNEL, ONESHOT_CHANNEL, COMMAND_PREFIX, \
+    role_dict_reaction
 import sys
 from discord.ext import tasks
 from datetime import datetime, timedelta
@@ -10,9 +11,8 @@ from utils.event_handlers import *
 # Setup logger
 logger = setup_logger(__name__)
 
-
 # Instantiate the bot
-bot = commands.Bot(command_prefix=COMMAND_PREFIX, intents=discord.Intents.all()) # Initialize bot with a command prefix
+bot = commands.Bot(command_prefix=COMMAND_PREFIX, intents=discord.Intents.all())  # Initialize bot with a command prefix
 
 
 # Attach event handlers
@@ -20,13 +20,16 @@ bot = commands.Bot(command_prefix=COMMAND_PREFIX, intents=discord.Intents.all())
 async def on_ready():
     await bot_ready(bot)
 
+
 @bot.event
 async def on_disconnect():
     await bot_disconnect(bot)
 
+
 @bot.event
 async def on_resumed():
     await bot_resumed(bot)
+
 
 @bot.event
 async def on_command_error(ctx, error):
@@ -34,93 +37,16 @@ async def on_command_error(ctx, error):
 
 @bot.event
 async def on_raw_reaction_add(payload):
-    logger.info(payload.emoji)
-    logger.info(repr(payload.emoji))
     assignmentlog = bot.get_channel(1219030657955794954)
     channel_id = payload.channel_id
     if payload.user_id != BOT_ID:  # Checks so it's not the bot reacting
-        emoji_repr = repr(payload.emoji)
-        if channel_id == ASSIGNMENT_CHANNEL: # everything in the assignment channel inside this if clause
-            data, row_name = await sh.getmessageid(payload.message_id)
-            if f"<@{payload.user_id}>" == data[4]:
-                role = data[2]
-                if emoji_repr == "<PartialEmoji animated=False name='✅' id=None>":
-                    await remove_reaction(bot, payload.channel_id, payload.message_id, "❌", True)
-                    await remove_reaction(bot, payload.channel_id, payload.message_id, "✅", True)
-                    await reactionhelper(data, assignmentlog, "Working")
-                    current_time = datetime.now().date().strftime("%Y-%m-%d")
-                    await sh.storetime(row_name, current_time)
-                elif emoji_repr == "<PartialEmoji animated=False name='🥂' id=None>":
-                    if role in role_dict_reaction:
-                        role = role_dict_reaction[role]
-                    await assignmentlog.send(
-                        f"{await sh.getchannelid(data[0])} | CH {data[1]} | {role} | **Done** | {data[4]}")
-                    # if role == "UPD":
-                    #    await sh.write(data, "")
-                    # else:
-                    #    await sh.write(data, "Done")
-                    await sh.write(data, "Done")
-                    await sh.delete_row(row_name)  # clear message data
-                    await remove_reaction(bot, payload.channel_id, payload.message_id, "🥂", False)
-                    await asyncio.sleep(120)
-                    await delete_message(bot, payload.channel_id, payload.message_id)
-                elif emoji_repr == "<PartialEmoji animated=False name='❌' id=None>":
-                    await reactionhelper(data, assignmentlog, "Declined")
-                    await delete_message(bot ,payload.channel_id, payload.message_id)
-                    await sh.delete_row(row_name)  # clear
-                elif emoji_repr == "<PartialEmoji animated=False name='💣' id=None>":
-                    guild = bot.get_guild(payload.guild_id)
-                    required_role = discord.utils.get(guild.roles, name="Tavern Keeper")
-                    member = guild.get_member(payload.user_id)
-                    logger.info(member.roles)
-                    if required_role in member.roles:
-                        await delete_message(bot, payload.channel_id, payload.message_id)
-                        await sh.write(data, "")
-                        await sh.delete_row(row_name)
-                        #await assignmentlog.send(f"{await sh.getchannelid(data[0])} | CH {data[1]} | {role} | **Deleted** | {data[4]}")
-        elif channel_id == CHECKUP_CHANNEL: # every reaction in the Hydrometer channel
-            data, row_name = await sh.getmessageid_due_date(payload.message_id)
-            if f"<@{payload.user_id}>" == data[4]:
-
-                if emoji_repr == "<PartialEmoji animated=False name='✅' id=None>":
-                    await delete_message(bot, payload.channel_id, payload.message_id, delete_after=60)
-                    await remove_reaction(bot, payload.channel_id, payload.message_id, "❌", False)
-                    await remove_reaction(bot, payload.channel_id, payload.message_id, "<:no:1225574648088105040>", False)
-                if emoji_repr == "<PartialEmoji animated=False name='no' id=1225574648088105040>":
-                    row = row_name
-                    user = bot.get_user(payload.user_id)
-                    role = data[2]
-                    original_date = data[5]
-                    if role in role_dict_reaction:
-                        role = role_dict_reaction[role]
-                    date, msg = await select_date(bot,user, data[0], data[1], role, f"<@{payload.user_id}>")
-                    due_date = date - timedelta(days=4)
-                    await sh.storetime(row, due_date)
-                    await sh.remove_due_date(row)
-                    await delete_message(bot, payload.channel_id, payload.message_id)
-                    await assignmentlog.send(
-                        f"<@{payload.user_id}> has **extended** the due date for {data[0]} CH {data[1]} (Role: {role}) from {original_date} to **{date}**.\n"
-                        f"Reason: {msg.content}")
-                elif emoji_repr == "<PartialEmoji animated=False name='❌' id=None>":
-                    await reactionhelper(data, assignmentlog, "Declined")
-                    await delete_message(bot, payload.channel_id, payload.message_id)
-                    await sh.delete_row(row_name)  # clear
+        if channel_id == ASSIGNMENT_CHANNEL:
+            await assignment_reaction(bot, payload, assignmentlog)
+        elif channel_id == CHECKUP_CHANNEL:  # every reaction in the Hydrometer channel
+            await checkup_reaction(bot, payload, assignmentlog)
         elif channel_id == ONESHOT_CHANNEL:
-            if emoji_repr == "<PartialEmoji animated=False name='BunKill' id=1218766575302348891>":
-                role = "RP"
-            elif emoji_repr == "<PartialEmoji animated=False name='cocosideeye' id=1219500662799208458>":
-                role = "TL"
-            elif emoji_repr == "<PartialEmoji animated=False name='communistecommunist' id=1219519556305948723>":
-                role = "PR"
-            elif emoji_repr == "<PartialEmoji animated=False name='Kokopium' id=1219519277783322647>":
-                role = "CLRD"
-            elif emoji_repr == "<PartialEmoji animated=False name='mingmingsmirk' id=1224929840633741312>":
-                role = "TS"
-            elif emoji_repr == "<PartialEmoji animated=False name='JerryShocked' id=1218766878479093872>":
-                role = "QC"
-            else:
-                return
-            await sh.oneshot(payload.message_id, role)
+            await oneshot_reaction(bot, payload, assignmentlog)
+
 
 @bot.event
 async def on_member_join(member):
@@ -137,11 +63,11 @@ async def check_old_entries():
 # Main function to run the bot
 async def main():
     try:
-        await bot.load_extension('cogs.bot_commands') # Load your commands extension
-        await bot.load_extension('cogs.help_command') # Load your help command extension
+        await bot.load_extension('cogs.bot_commands')  # Load your commands extension
+        await bot.load_extension('cogs.help_command')  # Load your help command extension
 
-        check_old_entries.start() # Start the looping task
-        await bot.start(TOKEN) # Start the bot
+        check_old_entries.start()  # Start the looping task
+        await bot.start(TOKEN)  # Start the bot
 
     except KeyboardInterrupt:
         await bot.close()
@@ -155,7 +81,118 @@ def handle_exception(exc_type, exc_value, exc_traceback):
         return
     logger.error("Uncaught exception", exc_info=(exc_type, exc_value, exc_traceback))
 
+
 sys.excepthook = handle_exception
+
+
+async def checkup_reaction(botins, payload, assignmentlog):
+    """
+        Handles reactions added to checkup messages in Discord.
+
+        This function is triggered when a reaction is added to a checkup message. It checks the type of reaction and performs different actions accordingly:
+        - '✅': Deletes the checkup message and removes certain reactions.
+        - 'no': Extends the due date for the assignment related to the checkup message, sends a message to the assignment log about the extension, and deletes the checkup message.
+
+        Args:
+            botins (Bot): The bot instance.
+            payload (Payload): The payload of the reaction add event.
+            assignmentlog (TextChannel): The Discord channel where assignment logs are sent.
+        """
+    emoji_repr = repr(payload.emoji)
+    data, row_name = await sh.getmessageid_due_date(payload.message_id)
+    if f"<@{payload.user_id}>" == data[4]:
+        if emoji_repr == "<PartialEmoji animated=False name='✅' id=None>":
+            await delete_message(botins, payload.channel_id, payload.message_id, delete_after=60)
+            await remove_reaction(botins, payload.channel_id, payload.message_id, "❌", False)
+            await remove_reaction(botins, payload.channel_id, payload.message_id, "<:no:1225574648088105040>", False)
+        if emoji_repr == "<PartialEmoji animated=False name='no' id=1225574648088105040>":
+            row = row_name
+            user = botins.get_user(payload.user_id)
+            role = data[2]
+            original_date = data[5]
+            if role in role_dict_reaction:
+                role = role_dict_reaction[role]
+            date, msg = await select_date(botins, user, data[0], data[1], role, f"<@{payload.user_id}>")
+            due_date = date - timedelta(days=4)
+            await sh.storetime(row, due_date)
+            await sh.remove_due_date(row)
+            await delete_message(botins, payload.channel_id, payload.message_id)
+            await assignmentlog.send(
+                f"<@{payload.user_id}> has **extended** the due date for {data[0]} CH {data[1]} (Role: {role}) from {original_date} to **{date}**.\n"
+                f"Reason: {msg.content}")
+
+
+async def assignment_reaction(botins, payload, assignmentlog):
+    """
+        Handles reactions added to assignment messages in Discord.
+
+        This function is triggered when a reaction is added to an assignment message. It checks the type of reaction and performs different actions accordingly:
+        - '✅': Marks the assignment as "Working" and stores the current time.
+        - '🥂': Marks the assignment as "Done", sends a completion message to the assignment log, and deletes the assignment message after a delay.
+        - '❌': Marks the assignment as "Declined" and deletes the assignment message.
+        - '💣': If the user has the "Tavern Keeper" role, deletes the assignment message.
+
+        Args:
+            botins (Bot): The bot instance.
+            payload (Payload): The payload of the reaction add event.
+            assignmentlog (TextChannel): The Discord channel where assignment logs are sent.
+        """
+    emoji_repr = repr(payload.emoji)
+    data, row_name = await sh.getmessageid(payload.message_id)
+    if f"<@{payload.user_id}>" == data[4]:
+        role = data[2]
+        if emoji_repr == "<PartialEmoji animated=False name='✅' id=None>":
+            await remove_reaction(botins, payload.channel_id, payload.message_id, "❌", True)
+            await remove_reaction(botins, payload.channel_id, payload.message_id, "✅", True)
+            await reactionhelper(data, assignmentlog, "Working")
+            current_time = datetime.now().date().strftime("%Y-%m-%d")
+            await sh.storetime(row_name, current_time)
+        elif emoji_repr == "<PartialEmoji animated=False name='🥂' id=None>":
+            if role in role_dict_reaction:
+                role = role_dict_reaction[role]
+            await assignmentlog.send(
+                f"{await sh.getchannelid(data[0])} | CH {data[1]} | {role} | **Done** | {data[4]}")
+            # if role == "UPD":
+            #    await sh.write(data, "")
+            # else:
+            #    await sh.write(data, "Done")
+            await sh.write(data, "Done")
+            await sh.delete_row(row_name)  # clear message data
+            await remove_reaction(botins, payload.channel_id, payload.message_id, "🥂", False)
+            await asyncio.sleep(120)
+            await delete_message(botins, payload.channel_id, payload.message_id)
+        elif emoji_repr == "<PartialEmoji animated=False name='❌' id=None>":
+            await reactionhelper(data, assignmentlog, "Declined")
+            await delete_message(botins, payload.channel_id, payload.message_id)
+            await sh.delete_row(row_name)  # clear
+        elif emoji_repr == "<PartialEmoji animated=False name='💣' id=None>":
+            guild = botins.get_guild(payload.guild_id)
+            required_role = discord.utils.get(guild.roles, name="Tavern Keeper")
+            member = guild.get_member(payload.user_id)
+            logger.info(member.roles)
+            if required_role in member.roles:
+                await delete_message(botins, payload.channel_id, payload.message_id)
+                await sh.write(data, "")
+                await sh.delete_row(row_name)
+
+
+async def oneshot_reaction(botins, payload, assignmentlog):
+    emoji_repr = repr(payload.emoji)
+    if emoji_repr == "<PartialEmoji animated=False name='BunKill' id=1218766575302348891>":
+        role = "RP"
+    elif emoji_repr == "<PartialEmoji animated=False name='cocosideeye' id=1219500662799208458>":
+        role = "TL"
+    elif emoji_repr == "<PartialEmoji animated=False name='communistecommunist' id=1219519556305948723>":
+        role = "PR"
+    elif emoji_repr == "<PartialEmoji animated=False name='Kokopium' id=1219519277783322647>":
+        role = "CLRD"
+    elif emoji_repr == "<PartialEmoji animated=False name='mingmingsmirk' id=1224929840633741312>":
+        role = "TS"
+    elif emoji_repr == "<PartialEmoji animated=False name='JerryShocked' id=1218766878479093872>":
+        role = "QC"
+    else:
+        return
+    # await sh.oneshot(payload.message_id, role)
 
 
 if __name__ == '__main__':
